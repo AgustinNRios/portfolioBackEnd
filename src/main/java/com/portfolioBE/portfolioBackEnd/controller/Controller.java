69c80c9d4bfbfe92a2c635841bed2ed6dea78f5e
Agustin Rios <agrios@frba.utn.edu.ar>
2023-04-08 15:25:52 -0300

    se corrigieron errores y se cambio la en application.properties  el puerto a 3308

--- src/main/java/com/portfolioBE/portfolioBackEnd/controller/Controller.java
+++ src/main/java/com/portfolioBE/portfolioBackEnd/controller/Controller.java
@@ -3,29 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.portfolioBE.portfolioBackEnd.controller;
-import com.portfolioBE.portfolioBackEnd.model.Educacion;
+
 import com.portfolioBE.portfolioBackEnd.service.IEducacionService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
+
+import com.portfolioBE.portfolioBackEnd.model.Educacion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+/*
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
+*/
 
 @RestController
 public class Controller {
 
-        //implementacion de la interface
-    //private IEducacionService educServ;
-    //List<Educacion> listaEducacion = new ArrayList();
     /*
-
+    List<Educacion> listaEducacion = new ArrayList();
+    
     @GetMapping("/educacion")
     public String decirChau (@RequestParam Long id,
             @RequestParam String img,
@@ -36,7 +38,21 @@ public class Controller {
     {
         return "id: "+id+" institucion: "+institucion;
     }
+    
+    @PostMapping("new/educacion")
+    public void agregarEducacion(@RequestBody Educacion educ)
+    {
+        listaEducacion.add(educ);
+    }
+    
+    @GetMapping("/ver/educacion")
+    @ResponseBody
+    public List<Educacion> verEducacion()
+    {
+        return listaEducacion;
+    }
     */
+    
     @Autowired
     private IEducacionService educServ;
         
