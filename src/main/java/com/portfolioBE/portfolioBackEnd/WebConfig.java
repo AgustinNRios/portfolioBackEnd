6ebfa68fc79589152406cee1835d85ba78c2dffe
Agustin Rios <agrios@frba.utn.edu.ar>
2023-05-03 13:23:37 -0300

    cambio en los Cors en WebConfig

--- src/main/java/com/portfolioBE/portfolioBackEnd/WebConfig.java
+++ src/main/java/com/portfolioBE/portfolioBackEnd/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("https://portauthar528folio.web.app");//"/**"
     } 
     
 }
